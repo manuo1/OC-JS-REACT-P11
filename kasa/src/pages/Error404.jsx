19f22b43ab7f99fac0b5151eeab911b0537e39e0
Emmanuel Oudot <manuo1@free.fr>
2025-06-03 20:14:45 +0200

    feat(error404): created error404

--- kasa/src/pages/Error404.jsx
+++ kasa/src/pages/Error404.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import styles from "./Error404.module.scss";
 
 export default function Error404() {
   return (
-    <div className="not-found">
-      <h1 className="not-found__title">404</h1>
-      <p className="not-found__message">Oups ! La page que vous demandez n'existe pas.</p>
-      <Link className="not-found__link" to="/">Retourner sur la page d’accueil</Link>
+    <div className={styles.notFound}>
+      <h1 className={styles.title}>404</h1>
+      <p className={styles.message}>Oups ! La page que vous demandez n'existe pas.</p>
+      <Link className={styles.link} to="/">Retourner sur la page d’accueil</Link>
     </div>
   );
 }
