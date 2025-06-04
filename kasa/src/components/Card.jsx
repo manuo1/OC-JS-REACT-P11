892b11eed808ec715165212cf49b2f0669419993
Emmanuel Oudot <manuo1@free.fr>
2025-06-04 13:35:59 +0200

    feat: add fallback image logic to CardGrid for broken image links

--- kasa/src/components/Card.jsx
+++ kasa/src/components/Card.jsx
@@ -1,10 +1,19 @@
 import { Link } from "react-router-dom";
 import styles from "./Card.module.scss";
+import fallbackImage from '../assets/default-fallback-image.png';
 
 function Card({ id, title, cover }) {
   return (
     <Link to={`/logement/${id}`} className={styles.card}>
-      <img src={cover} alt={title} className={styles.image} />
+      <img
+        src={cover}
+        alt={title}
+        className={styles.image}
+        onError={(e) => {
+          console.warn("Cover Image failed to load for id :", id);
+          e.target.src = fallbackImage;
+        }}
+      />
       <div className={styles.gradient}></div>
       <div className={styles.overlay}>
         <h2 className={styles.title}>{title}</h2>
