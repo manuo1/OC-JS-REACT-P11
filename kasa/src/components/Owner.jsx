124da96763e64cfe9bffd83d542e110ae9e6b022
Emmanuel Oudot <manuo1@free.fr>
2025-06-05 11:04:23 +0200

    feat(owner): create Owner component to display host name and picture

---
+++ kasa/src/components/Owner.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Owner.module.scss';
+
+function Owner({ name, picture }) {
+  return (
+    <div className={styles.owner}>
+      <div className={styles.name}>
+        {name.split(' ').map((part, i) => (
+          <span key={i}>{part}</span>
+        ))}
+      </div>
+      <img src={picture} alt={`Photo de ${name}`} className={styles.picture} />
+    </div>
+  );
+}
+
+export default Owner;
